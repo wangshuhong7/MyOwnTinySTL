6fce964c06220019c7e3e52f07ed9c921eaba357
wangshuhong7 <wangshuhong7@hust.edu.cn>
2020-10-16 21:58:05 +0800

    add a new file "alloc.h"

--- myTinySTL/alloc.h
+++ myTinySTL/alloc.h
@@ -4,22 +4,23 @@
 #include<cstdlib>
 namespace myTinySTL {
 
-	enum EAign { ALIGN = 8 };//小型区块的上调边界
-	enum EMaxBytes { MAXBYTES = 128 };//上限，超过次上限直接调用malloc更佳
-	enum ENFreeLists { NFREELISTS = (EMaxBytes::MAXBYTES / EAign::ALIGN) };//有多少个区块，就要有多少freelist来保证其回收
-	enum ENObjs { NOBJS = 20 };//每次增添的节点数
-
-	//freelists的节点构造
-	union obj
-	{//利用union节省空间，这个区块既可以放下一块的指针，又可以存放数据。
-		//union共享内存，其大小取决于占用空间最大的一个成员的大小。其内可以定有多种不同的数据类型
-		union obj *next;
-		char client[1];
-	};
-
+	
 	class alloc {
 
 	private:
+		enum EAign { ALIGN = 8 };//小型区块的上调边界
+		enum EMaxBytes { MAXBYTES = 128 };//上限，超过次上限直接调用malloc更佳
+		enum ENFreeLists { NFREELISTS = (EMaxBytes::MAXBYTES / EAign::ALIGN) };//有多少个区块，就要有多少freelist来保证其回收
+		enum ENObjs { NOBJS = 20 };//每次增添的节点数
+
+		//freelists的节点构造
+		union obj
+		{//利用union节省空间，这个区块既可以放下一块的指针，又可以存放数据。
+			//union共享内存，其大小取决于占用空间最大的一个成员的大小。其内可以定有多种不同的数据类型
+			union obj *next;
+			char client[1];
+		};
+
 		//使用静态的原因是这些值都是唯一的。一旦改变，全局就跟着改变了
 		static char *start_free;//内存池的起始位置
 		static char *end_free;//内存池的结束位置
@@ -51,11 +52,46 @@ namespace myTinySTL {
 	char* alloc::start_free = 0;
 	char* alloc::end_free = 0;
 	size_t alloc::heap_size = 0;
-	obj* alloc::free_list[ENFreeLists::NFREELISTS] = {
-		nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,
-		nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,
+	alloc::obj *alloc::free_list[ENFreeLists::NFREELISTS] = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	};
-	
+
+	void *alloc::allocate(size_t bytes) {
+		if (bytes > EMaxBytes::MAXBYTES) {
+			return malloc(bytes);
+		}
+		size_t index = FREELIST_INDEX(bytes);
+		obj *list = free_list[index];
+		if (list) {
+			//次list还有空间
+			free_list[index] = list->next;
+			return list;
+		}
+		else {
+			//次list没有足够空间，要从内存池取空间
+			return refill(ROUND_UP(bytes));
+		}
+	}
+
+	void alloc::deallocate(void *ptr, size_t bytes) {
+		if (bytes > EMaxBytes::MAXBYTES) {
+			free(ptr);
+		}
+		else {
+			obj *node = static_cast<obj*>(ptr);
+			size_t index = FREELIST_INDEX(bytes);
+			node->next = free_list[index];
+			free_list[index] = node;
+		}
+	}
+
+	void *alloc::reallocate(void *ptr, size_t old_sz, size_t new_sz) {
+		deallocate(ptr, old_sz);
+		ptr = allocate(new_sz);
+	}
+	void *alloc::refill(size_t n) {
+
+	}
 
 }
 
